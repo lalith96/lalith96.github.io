0f96d36787e11182ded22f6c81f680c82ef2f1bc
lalith96 <lalithkasa31dec@gmail.com>
2019-10-15 19:14:43 +0530

    Added heap sort.

--- sorting.c
+++ sorting.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#define parent(i) (i-1)/2
+#define left(i) (2*i)+1
+#define right(i) (2*i)+2
 
 int swap(int *a, int *b){
     int temp = *a;
@@ -30,54 +33,81 @@ int merge(int a[],int start,int mid,int end){
     for(int i=0;i<n2;i++){
         right[i] = a[mid+i+1];
     }
-    i=0;
-    j=0;
-    k=start;
-    while(i<n1 && j<n2){
-        if(left[i]<=right[j]){
-            a[k++] = left[i++];
-        }
-        else {
-            a[k++] = right[j++];
-        }
+    i=0;    
+    j=0;    
+    k=start;    
+    while(i<n1 && j<n2){    
+        if(left[i]<=right[j]){  
+            a[k++] = left[i++]; 
+        }   
+        else {  
+            a[k++] = right[j++];    
+        }   
+    }   
+    while(i<n1){    
+        a[k++] = left[i++]; 
+    }   
+    while(j<n2){    
+        a[k++] = right[j++];    
+    }   
+}   
+
+int mergesort(int a[],int start,int end){   
+    int mid;    
+    if(start<end){  
+        mid = (start + end)/2;  
+        mergesort(a,start,mid); 
+        mergesort(a,mid+1,end); 
+        merge(a,start,mid,end); 
+    }   
+}   
+
+int partition(int a[],int start,int end){   
+    int i,j,pivot;  
+    pivot = a[end]; 
+    j=start-1;  
+    for(int i=start;i<end;i++){ 
+        if(a[i]<pivot){ 
+            j++;    
+            swap(&a[j],&a[i]);  
+        }   
+    }   
+    swap(&a[j+1],&a[end]);  
+    return j+1; 
+}   
+
+int quicksort(int a[],int start,int end){   
+    if(start<end){  
+        int p = partition(a,start,end); 
+        quicksort(a,start,p-1); 
+        quicksort(a,p+1,end);   
+    }   
+}   
+
+maxheapify(int a,int i,int length){   
+    int left = left(i);
+    int right = right(i);
+    int largest = i;
+    if(left<length && a[i]<a[left]){
+        largest = left;
+    }else if(right<length && a[largest]<a[right]){
+        largest = right;
     }
-    while(i<n1){
-        a[k++] = left[i++];
-    }
-    while(j<n2){
-        a[k++] = right[j++];
+    if(largest!=i){
+        swap(&a[i],a[largest]);
+        maxheapify(a,largest,length);
     }
 }
 
-int mergesort(int a[],int start,int end){
-    int mid;
-    if(start<end){
-        mid = (start + end)/2;
-        mergesort(a,start,mid);
-        mergesort(a,mid+1,end);
-        merge(a,start,mid,end);
-    }
+buildmaxheap(int a,int n){
+    for()
 }
 
-int partition(int a[],int start,int end){
-    int i,j,pivot;
-    pivot = a[end];
-    j=start-1;
-    for(int i=start;i<end;i++){
-        if(a[i]<pivot){
-            j++;
-            swap(&a[j],&a[i]);
-        }
-    }
-    swap(&a[j+1],&a[end]);
-    return j+1;
-}
-
-int quicksort(int a[],int start,int end){
-    if(start<end){
-        int p = partition(a,start,end);
-        quicksort(a,start,p-1);
-        quicksort(a,p+1,end);
+heapsort(int a[],int n){
+    buildmaxheap(a,n);
+    for(int i=n;i>1;i--){
+        swap(&a[1],&a[i]);
+        maxheapify(a,1,n);
     }
 }
 
@@ -88,7 +118,7 @@ int main()
     for(int i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
-    quicksort(a,0,n-1);
+    heapsort(a,n-1);
     for(int i=0;i<n;i++){
         printf("%d ",a[i]);
     }
